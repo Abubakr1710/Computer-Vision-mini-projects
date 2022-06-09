2553dc3e1c97d6ea4cac16e55e83b7fc531083af
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-06-09 05:50:20 -0700

    *

--- meme_creator.py
+++ meme_creator.py
@@ -2,7 +2,8 @@ import cv2
 import matplotlib.pyplot as plt
 img=cv2.imread('img/minion.jpg')
 
-m = cv2.putText(img,'SPACE and COMMA when I forgot them 500 line of code', (95,15), cv2.FONT_HERSHEY_COMPLEX,0.7, (255,255,255))
+m = cv2.putText(img,'SPACE and COMMA when I forgot them 500 line of code', org=(0,20), fontFace=cv2.FONT_HERSHEY_TRIPLEX,fontScale=0.6,color=(255,255,255), thickness=1)
+mdown= cv2.putText(m,'Do not forget them', org=(350, 345), fontFace=cv2.FONT_HERSHEY_TRIPLEX,fontScale=0.7,color=(255,255,255), thickness=1)
 path='img/meme.png'
 cv2.imwrite(path,m)
 meme = cv2.imread('img/meme.png')
